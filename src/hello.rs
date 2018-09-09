dcc782c1dc1f905ede9fc39bc4dc0db81e668940
Josh Peak <neozenith.dev@gmail.com>
2018-09-09 16:40:46 +1000

    add a basic rust source file

---
+++ src/hello.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // The statements here will be executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+    println!("1 + 2 = {}", 1u32 + 2);
+
+    // Integer subtraction
+    println!("1 - 2 = {}", 1i32 - 2);
+}
